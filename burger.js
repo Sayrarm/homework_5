a970eac1c481bd2fd764161431026a8179b67ca9
Sayrarm <56771509+Sayrarm@users.noreply.github.com>
2025-11-28 17:46:52 +0300

    feat: add animation for modal window

--- burger.js
+++ burger.js
@@ -1,7 +1,18 @@
 let modal = document.getElementById('modal_nav');
 let burgerBtn = document.getElementById('burger');
 
-burgerBtn.addEventListener('click',function(){
-    modal.classList.toggle('active');
+burgerBtn.addEventListener('click', function() {
+    if (modal.classList.contains('active')) {
+        modal.style.transform = 'translateY(-100%)';
+        setTimeout(() => {
+            modal.classList.remove('active');
+        }, 300);
+    } else {
+        modal.classList.add('active');
+        // Небольшая задержка чтобы успел примениться display: flex
+        setTimeout(() => {
+            modal.style.transform = 'translateY(0)';
+        }, 10);
+    }
 });
 
