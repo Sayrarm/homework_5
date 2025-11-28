d12a0c5e8963a24d7a787c41536ad88ddb51741c
Sayrarm <56771509+Sayrarm@users.noreply.github.com>
2025-11-28 17:20:06 +0300

    feat: add script for burger-button

---
+++ burger.js
@@ -0,0 +1,7 @@
+let modal = document.getElementById('modal_nav');
+let burgerBtn = document.getElementById('burgerBtn');
+
+burgerBtn.addEventListener('click',function(){
+    modal.classList.toggle('active');
+});
+
